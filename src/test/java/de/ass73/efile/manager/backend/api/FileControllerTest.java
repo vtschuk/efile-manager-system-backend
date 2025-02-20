db69aaeecbae332726b2ea0da1a8eb808b02ff61
xgarnet <vt7970170@gmail.com>
2025-02-20 15:57:02 +0100

    add README.MD

--- src/test/java/de/ass73/efile/manager/backend/api/FileControllerTest.java
+++ src/test/java/de/ass73/efile/manager/backend/api/FileControllerTest.java
@@ -1,15 +1,20 @@
 package de.ass73.efile.manager.backend.api;
 
+import org.junit.jupiter.api.Test;
+
 public class FileControllerTest {
 
+    @Test
     public void uploadFileTest() {
 
     }
 
+    @Test
     public void downloadFileTest() {
 
     }
 
+    @Test
     public void deleteFileTest() {
 
     }
